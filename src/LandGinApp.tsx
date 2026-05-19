33f05b991a8b64b63765892c97cdcc13b0d6d91d
Adrian Fayos <adrianfayos1234@gmail.com>
2026-05-19 18:23:56 -0300

    Create layouts and components for the main app structure Update LandGinApp with new structure

--- src/LandGinApp.tsx
+++ src/LandGinApp.tsx
@@ -1,3 +1,21 @@
+import { Footer } from './components/layout/Footer'
+import { Navbar } from './components/layout/Navbar'
+import { CTA } from './components/sections/CTA'
+import { Features } from './components/sections/Features'
+import { Hero } from './components/sections/Hero'
+import { Testimonials } from './components/sections/Testimonials'
+
 export const LandGinApp = () => {
-  return <div>LandGinApp</div>
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <Testimonials />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
+  )
 }
