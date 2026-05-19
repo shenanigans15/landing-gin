c07fb9888e909b99cdd6af57d73728a58cc0a25a
Adrian Fayos <adrianfayos1234@gmail.com>
2026-05-19 20:47:06 -0300

    Add font and images to public Update Navbar with structure Add shadcn button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,3 +1,27 @@
+import { Button } from '../ui/button'
+
 export const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <header>
+      <div>
+        {/* Logo + Marca */}
+        <div>
+          <img />
+          <div>
+            <h1> Brújula Vikinga</h1>
+            <span>Handcrafted Gin</span>
+          </div>
+        </div>
+
+        {/* Links */}
+        <nav>
+          <a href="">Inicio</a>
+          <a href="">Características</a>
+          <a href="">Opiniones</a>
+        </nav>
+        {/* CTA */}
+        <Button>Comprar</Button>
+      </div>
+    </header>
+  )
 }
